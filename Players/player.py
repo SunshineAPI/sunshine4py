386b46be7fc857120f77f4dc70e19cdb360e06ab
BuildTools <preitek@gmail.com>
2015-08-10 03:13:16 -0400

    Added player module, player stats, json parsing for player data

--- Players/player.py
+++ Players/player.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python
 
 import unirest
+import json
 
 class SunshinePlayer:
+    def setPlayerAttributes(self, data):
+        for key, value in data.items():
+            setattr(self, str(key), value)
+            if isinstance(value, dict):
+                data = value
+                self.setPlayerAttributes(data)
+
     def __init__(self, name, url):
-        self.playerdata = unirest.get(url, header={"Content-Type":"application/json"})
-        return self.playerdata
+        self.player_data = json.dumps(unirest.get('http://' + url, header={"Content-Type":"application/json"}).body)
+        parsed_data = json.loads(self.player_data)
+        self.setPlayerAttributes(parsed_data)
--- sunshine.py
+++ sunshine.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
-import os
+from os.path import join
 import json
 import urllib
 import socket
-import Players
+from Players.player import SunshinePlayer
 try:
     from pip import get_installed_distributions
     packages = get_installed_distributions()
@@ -12,20 +12,32 @@ try:
     if 'unirest' in packages_list:
         import unirest
 except ImportError as e:
-    print 'Cannot import Unirest, either you don\'t have it installed, \n \
+    print str(e) + ', Cannot import Unirest, either you don\'t have it installed, \n \
            or it isn\'t installed properly. ' + str(e)
 except Exception:
     print 'Unexpected exception'
 
 class Sunshine:
     def __init__(self, server, port):
+        self.server = server
+        self.port = port
         if not isinstance(server, str):
             raise TypeError("Server address must be str")
         elif not isinstance(port, int):
             if isinstance(port, str):
                 try:
-                    port = int(port)
+                    self.port = int(port)
                 except Exception:
                     raise Exception('port number must be an int or str')
             else:
                 raise TypeError('port number must be an int or str')
+        self.server_connection = socket.socket(socket.AF_INET)
+        try:
+            self.server_connection.connect((server, port))
+            self.ip_addr = socket.gethostbyname(server)
+            print self.ip_addr
+        except Exception as e:
+            print str(e) + ', unable to connect to server'
+    def getPlayer(self, name):
+        player_url = join('{0}:{1}'.format(self.server, self.port), 'players', name)
+        return SunshinePlayer(name, player_url)
